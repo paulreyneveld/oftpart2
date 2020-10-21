8848ba2f9de23722a7e4c038cd9763f2bbc8fa94
Paul <63985305+paulreyneveld@users.noreply.github.com>
2020-10-21 12:34:45 -0700

    Updated through exercise 2.8

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,39 +1,68 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
+    const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas', number: '555-5555', id: 1 },
+    { name: 'Ada Lovelace', number: '555-5555', id: 2},
+    ]);
 
-  const addPerson = (event) => {
-      event.preventDefault()
-      const personObject = {
-          name: newName
-      }
-      setPersons(persons.concat(personObject))
-      setNewName('')
-  }
+    const [ newName, setNewName ] = useState('');
+    const [ newNumber, setNewNumber ] = useState('');
 
-  const handleNewPerson = (event) => {
-    setNewName(event.target.value)
-  }
+    const addPerson = (event) => {
+        event.preventDefault()
+        const personObject = {
+            name: newName,
+            number: newNumber,
+            id: persons.length + 1
+        }
 
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
+        let dummyBool = false;
+
+        for (const person in persons) {
+            if (persons[person].name.toLowerCase() === newName.toLowerCase()) {
+                dummyBool = true;
+                break;
+            }
+            else {
+                dummyBool = false;
+            }
+        }  
+
+        if (dummyBool) {
+            alert(`${newName} already exists in phonebook`);
+        }
+        else {
+            setPersons(persons.concat(personObject));
+            setNewName('');
+        }
+    }
+  
+    const handleNewPerson = (event) => {
+        setNewName(event.target.value);
+    }
+
+    const handleNewNumber = (event) => {
+        setNewNumber(event.target.value);
+    }
+
+    return (
         <div>
-          name: <input value={newName} onChange={handleNewPerson} />
+            <h2>Phonebook</h2>
+            <form onSubmit={addPerson}>
+            <div>
+                name: <input value={newName} onChange={handleNewPerson} />
+                <br />
+                number: <input value={newNumber} onChange={handleNewNumber} />
+            </div>
+            <div>
+                <button type="submit">add</button>
+            </div>
+            </form>
+            <h2>Numbers</h2>
+                {persons.map(person => <p key={person.id}> {person.name} {person.number} </p>)}
         </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {persons.map(person => <p> {person.name} </p>)}
-    </div>
-  )
+    )
 }
 
 export default App
