a108590fd696a4b4b15d7683a76ed08b2972f341
Paul <63985305+paulreyneveld@users.noreply.github.com>
2020-10-20 23:34:56 -0700

    Added functionality for part 2.6

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -6,19 +6,32 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const addPerson = (event) => {
+      event.preventDefault()
+      const personObject = {
+          name: newName
+      }
+      setPersons(persons.concat(personObject))
+      setNewName('')
+  }
+
+  const handleNewPerson = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNewPerson} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => <p> {person.name} </p>)}
     </div>
   )
 }
