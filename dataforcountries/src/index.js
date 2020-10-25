b34821dc8970e606b8139e128805eabb435bc925
Paul <63985305+paulreyneveld@users.noreply.github.com>
2020-10-25 11:55:57 -0700

    Added functionality through 2.14

--- dataforcountries/src/index.js
+++ dataforcountries/src/index.js
@@ -2,64 +2,90 @@ import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 
-const App = () => {
-const [ countryInfo, setCountryInfo ] = useState([]);
-const [ search, setSearch ] = useState('');
-const [ foundCountries, setFoundCountries ] = useState([]);
+const CountryList = ({ show, country }) => {
+  const [ weatherInfo, setWeatherInfo ] = useState([])
 
-const getCountryInfoHook = () => {
-  axios.get("https://restcountries.eu/rest/v2/all")
+  const getWeatherHook = () => {
+    const apiDestination = `http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_API_KEY}&query=${country.capital}`
+    axios.get(apiDestination)
     .then(response => {
-      setCountryInfo(response.data);
+      console.log(response);
+      setWeatherInfo(response.data);
     })
+    .catch(error => console.log(error));
+  }
+
+  useEffect(getWeatherHook, [country.name])
+  
+  if (show === true) {
+    return (
+      <>
+      <h2> {country.name}</h2>
+      <p> capital: {country.capital}</p>
+      <p> population: {country.population}</p>
+      <img alt="Country flag" src={country.flag} /> 
+      <h2>Languages</h2>
+      <ul>
+      {country.languages.map((language, index) => <li key={index}>{language.name}</li>)}
+      </ul>
+      </>
+    )
+  }
+  return (
+    <>
+    </>
+  )
 }
 
-useEffect(getCountryInfoHook, []);
+const App = () => {
+  const [ countryInfo, setCountryInfo ] = useState([]);
+  const [ search, setSearch ] = useState('');
+  const [ foundCountries, setFoundCountries ] = useState([]);
+  const [ singleCountry, setSingleCountry ] = useState([]);
+  const [ showValue, setShowValue ] = useState(false);
 
-const handleSearch = (event) => {
-  setSearch(event.target.value.toLowerCase());
-}
+  const getCountryInfoHook = () => {
+    axios.get("https://restcountries.eu/rest/v2/all")
+      .then(response => {
+        setCountryInfo(response.data);
+      })
+  }
 
-const conductSearchHook = () => {
-  const results = countryInfo
-    .filter(country => country.name.toLowerCase().includes(search));
-    setFoundCountries(results);
-    console.log(foundCountries);
-}
+  useEffect(getCountryInfoHook, []);
 
-useEffect(conductSearchHook, [search])
+  const handleSearch = (event) => {
+    setSearch(event.target.value.toLowerCase());
+    setShowValue(false);
+    setSingleCountry([]);
+  }
 
-const countryList = () => {
-  if (foundCountries.length === 0) {
-    return <p>No matches found</p>
+  const conductSearchHook = () => {
+    const results = countryInfo
+      .filter(country => country.name.toLowerCase().includes(search));
+      setFoundCountries(results);
   }
-  if (foundCountries.length === 1) {
-     return foundCountries.map((country, index) => 
-     <>
-     <h2 key={index}> {country.name}</h2>
-     <p> capital: {country.capital}</p>
-     <p> population: {country.population}</p>
-     <img alt="Country flag" src={country.flag} /> 
-     <h2>Languages</h2>
-     <ul>
-     {country.languages.map((language, index) => <li key={index}>{language.name}</li>)}
-     </ul>
-     </>
-     )
+
+  useEffect(conductSearchHook, [search])
+
+  const countryList = () => {
+    if (foundCountries.length > 10) {
+      return <p>Too many matches, specify another filter</p>
+    } 
+    else if (foundCountries.length <= 10 && foundCountries.length >= 1) {
+      return foundCountries.map((country, index) => <p key={index}>{country.name}<button onClick={() => { setShowValue(true);  setSingleCountry(country); }}>show</button></p>);
+    }
+    else {
+      return <p>No matches found</p>;
+    }
   }
-  else if (foundCountries.length <= 10 && foundCountries.length > 1) {
-    return foundCountries.map((country, index) => <p key={index}>{country.name}</p>);
-  }
-  else {
-    return <p>Too many matches, specify another filter</p>;
-  }
-}
 
   return (
     <div>
       <label>Find Countries: </label>
-      <input value={search} onChange={handleSearch} />
+      <input type="text" value={search} onChange={handleSearch} />
       {countryList()}
+      <CountryList show={showValue} country={singleCountry} /> 
+
     </div>
   )
 }
